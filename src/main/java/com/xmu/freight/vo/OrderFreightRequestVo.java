d2e1e3374fc95cbb9fac7e0db010f34487ee028e
hsx <1027056027@qq.com>
2019-12-10 17:21:38 +0800

    init

---
+++ src/main/java/com/xmu/freight/vo/OrderFreightRequestVo.java
@@ -0,0 +1,15 @@
+package com.xmu.freight.vo;
+
+import com.xmu.freight.standardDomain.Address;
+import com.xmu.freight.standardDomain.Goods;
+import com.xmu.freight.standardDomain.OrderItem;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class OrderFreightRequestVo {
+    private Address address;
+
+    private List<OrderItem> orderItemList;
+
+}
