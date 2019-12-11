3e4665a65cd37302d4cd140ff984515da2d38b2d
hsx1027056027 <1027056027@qq.com>
2019-12-11 22:05:16 +0800

    12.11基本完成,未测试

--- src/main/java/com/xmu/freight/controller/FreightControllerDto.java
+++ src/main/java/com/xmu/freight/controller/FreightControllerDto.java
@@ -6,24 +6,22 @@ import com.xmu.freight.domain.OrderItemDto;
 import com.xmu.freight.domain.ProductDto;
 import com.xmu.freight.domain.SpecialFreightDto;
 import com.xmu.freight.service.FreightService;
-import com.xmu.freight.service.impl.FreightServiceImpl;
 import com.xmu.freight.standardController.FreightController;
-import com.xmu.freight.standardDomain.OrderItem;
+import com.xmu.freight.standardDomain.DefaultPieceFreight;
+import com.xmu.freight.standardDomain.SpecialFreight;
 import com.xmu.freight.util.ResponseUtil;
 import com.xmu.freight.vo.OrderFreightRequestVo;
 import com.xmu.freight.vo.OrderItemVo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.annotation.Order;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
-import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/admin-freight")// /wx/order
+@RequestMapping("")// /wx/order
 
 public class FreightControllerDto implements FreightController {
     @Autowired
@@ -31,46 +29,6 @@ public class FreightControllerDto implements FreightController {
 
     @Autowired
     RestTemplate restTemplate;
-
-    @Override
-    public Object getDefaultFreights() {
-        return freightService.getDefaultFreights();
-    }
-
-    @Override
-    public Object getSpecialFreight() {
-        return freightService.getSpecialFreights();
-    }
-
-    @Override
-    public Object addDefaultFreights(@RequestBody DefaultFreightDto defaultFreightDto) {
-        return freightService.addDefaultFreight(defaultFreightDto);
-    }
-
-    @Override
-    public Object addSpecialFreight(@RequestBody SpecialFreightDto specialFreightDto) {
-        return freightService.addSpecialFreight(specialFreightDto);
-    }
-
-    @Override
-    public Object deleteDefaultFreight(String defaultFreightsId) {
-        return null;
-    }
-
-    @Override
-    public Object deleteSpecialFreight(String specialFreightsId) {
-        return null;
-    }
-
-    @Override
-    public Object updateSpecialFreight(String specialFreightsId) {
-        return null;
-    }
-
-    @Override
-    public Object updateDefaultFreight(String defaultFreightsId) {
-        return null;
-    }
 
     /**
      * 获得运费
@@ -89,5 +47,45 @@ public class FreightControllerDto implements FreightController {
         }
         Object retObj = ResponseUtil.ok(freightService.getFreight(OrderItemDtoList,orderFreightRequestVo.getAddress()));
         return retObj;
+    }
+
+    @Override
+    public Object getDefaultFreights() {
+        return null;
+    }
+
+    @Override
+    public Object getSpecialFreight() {
+        return null;
+    }
+
+    @Override
+    public Object addDefaultFreights(DefaultPieceFreight defaultPieceFreight) {
+        return null;
+    }
+
+    @Override
+    public Object addSpecialFreight(SpecialFreight specialFreight) {
+        return null;
+    }
+
+    @Override
+    public Object deleteDefaultFreight(Integer id) {
+        return null;
+    }
+
+    @Override
+    public Object deleteSpecialFreight(Integer id) {
+        return null;
+    }
+
+    @Override
+    public Object updateSpecialFreight(Integer id, SpecialFreight specialFreight) {
+        return null;
+    }
+
+    @Override
+    public Object updateDefaultFreight(Integer id, DefaultPieceFreight defaultPieceFreight) {
+        return null;
     }
 }
--- src/main/java/com/xmu/freight/standardController/FreightController.java
+++ src/main/java/com/xmu/freight/standardController/FreightController.java
@@ -1,15 +1,14 @@
 package com.xmu.freight.standardController;
 
-import com.xmu.freight.domain.DefaultFreightDto;
-import com.xmu.freight.domain.SpecialFreightDto;
+
+import com.xmu.freight.standardDomain.DefaultPieceFreight;
+import com.xmu.freight.standardDomain.SpecialFreight;
 import com.xmu.freight.vo.OrderFreightRequestVo;
-import org.apache.tomcat.util.http.ResponseUtil;
-import org.springframework.core.annotation.Order;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.NotNull;
 @RestController
-@RequestMapping("/admin-freight")// /wx/order
+@RequestMapping("/admin-freightSerice")// /wx/order
 
 public interface FreightController {
 
@@ -18,28 +17,28 @@ public interface FreightController {
     public Object getDefaultFreights();
 
 
-    @GetMapping("/specialFreight")
+    @GetMapping("/specialFreights")
     public Object getSpecialFreight();
 
 
     @PostMapping("/defaultFreights")
-    public Object addDefaultFreights(@RequestBody DefaultFreightDto defaultFreightDto);
+    public Object addDefaultFreights(DefaultPieceFreight defaultPieceFreight);
 
     @PostMapping("/specialFreight")
-    public Object addSpecialFreight(@RequestBody SpecialFreightDto specialFreightDto);
+    public Object addSpecialFreight(SpecialFreight specialFreight);
 
     @DeleteMapping("/defaultFreights/{id}")
-    public Object deleteDefaultFreight(@PathVariable("defaultFreightsId") String defaultFreightsId);
+    public Object deleteDefaultFreight(@PathVariable Integer id);
 
     @DeleteMapping("/specialFreights/{id}")
-    public Object deleteSpecialFreight(@PathVariable("specialFreightsId") String specialFreightsId);
+    public Object deleteSpecialFreight(@PathVariable Integer id);
 
     @PutMapping("/specialFreights/{id}")
-    public Object updateSpecialFreight(@PathVariable("specialFreightsId") String specialFreightsId);
+    public Object updateSpecialFreight(@PathVariable Integer id, @RequestBody SpecialFreight specialFreight);
 
     @DeleteMapping("/defaultFreights/{id}")
-    public Object updateDefaultFreight(@PathVariable("defaultFreightsId") String defaultFreightsId);
+    public Object updateDefaultFreight(@PathVariable Integer id, @RequestBody DefaultPieceFreight defaultPieceFreight);
 
-    @GetMapping("/freight/{orderId}")
+    @GetMapping("/freight/{orderid}")
     public Object getFreight(@RequestBody OrderFreightRequestVo orderFreightRequestVo);
 }
